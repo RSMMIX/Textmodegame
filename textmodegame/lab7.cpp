278474cd23443b54c6f1c4f2d5f7117bbae534ae
ppawnn <88606135+ppawnn@users.noreply.github.com>
2021-09-20 14:15:38 +0700

    Game

--- textmodegame/lab7.cpp
+++ textmodegame/lab7.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 #include<windows.h>
 #include<time.h>
 #include<conio.h>
@@ -95,4 +95,4 @@ int main()
 		Sleep(100);
 	} while (ch != 'x');
 	return 0;
-}
+}*/
